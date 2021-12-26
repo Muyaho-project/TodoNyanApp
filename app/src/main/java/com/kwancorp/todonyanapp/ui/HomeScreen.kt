122d1b83a3d1cac2b9ef086be28c59290ea89533
YunByungKwan <pk7955@naver.com>
2021-12-26 16:55:26 +0900

    feat: Home화면 ui 추가

--- app/src/main/java/com/kwancorp/todonyanapp/ui/HomeScreen.kt
+++ app/src/main/java/com/kwancorp/todonyanapp/ui/HomeScreen.kt
@@ -1,7 +1,38 @@
 package com.kwancorp.todonyanapp.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.kwancorp.todonyanapp.R
 
 @Composable
 fun HomeBody() {
+    Column {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Image(
+                painter = painterResource(R.drawable.bg_room),
+                modifier = Modifier
+                    .fillMaxWidth(),
+                contentScale = ContentScale.FillWidth,
+                contentDescription = null
+            )
+        }
+
+        Row {
+            Text("29일 목.")
+            Text("7개의 할 일")
+            Text("그룹순")
+            Text("시간순")
+        }
+    }
 }
