06ea5e81e6402c7e83c763d1a30d95cc468f663a
YunByungKwan <pk7955@naver.com>
2021-12-26 20:52:44 +0900

    feat: Cat화면 ui 추가

--- app/src/main/java/com/kwancorp/todonyanapp/ui/CatScreen.kt
+++ app/src/main/java/com/kwancorp/todonyanapp/ui/CatScreen.kt
@@ -1,9 +1,35 @@
 package com.kwancorp.todonyanapp.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.kwancorp.todonyanapp.R
 
 @Composable
 fun CatBody() {
-    Text("CatScreen")
+    Column {
+        Image(
+            painter = painterResource(R.drawable.bg_cat_room),
+            modifier = Modifier
+                .fillMaxWidth(),
+            contentScale = ContentScale.FillWidth,
+            contentDescription = null
+        )
+
+        Row {
+            Text("함께하는 중인 투두냥")
+            Text("3")
+        }
+
+        Row {
+            Text("만나고 싶은 투두냥")
+            Text("32")
+        }
+    }
 }
